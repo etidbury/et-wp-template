e36492ed65afee93d104c12982736aaa7930f194
etidbury <tidbury@live.com>
2017-01-17 15:11:09 +0000

    Initial commit - test

--- _install/git-remote-init.sh
+++ _install/git-remote-init.sh
@@ -3,7 +3,7 @@
 git init
 git add ../
 git add .
-git commit -am 'Initial commit'
+git commit -am 'Initial commit - test'
 curl -u 'etidbury' https://api.github.com/user/repos -d "{\"name\":\"$1\"}"
 git remote add origin "https://github.com/etidbury/$1.git"
 git push -u origin master
